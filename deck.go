ab7ece24aa885a3d47f9feaa286278e4665d6e25
Guy.GPL <poramutinb@hotmail.com>
2024-03-03 00:06:51 +0700

    create main.go and type deck

---
+++ deck.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSults := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, sult := range cardSults {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+sult)
+		}
+	}
+	return cards
+}
+
+func (d deck) deal(handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for i := range d {
+		index := r.Intn(len(d) - 1)
+		d[i], d[index] = d[index], d[i]
+	}
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) toBypeSlice() []byte {
+	return []byte(d.toString())
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, d.toBypeSlice(), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
